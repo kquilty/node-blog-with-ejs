9fe735ab7b3bcba863ef44afe1b95216af00a299
Kyle Quilty <kquilty@gmail.com>
2025-10-01 15:24:34 -0400

    cleanup

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const blogRoutes = require('./routes/blogRoutes');
 
 const app = express();
 
+// Use EJS as the view engine
 app.set('view engine', 'ejs');// by default, looks in "./views"
 
 // middleware & static files
@@ -13,10 +14,8 @@ app.use(morgan('dev'));//<------------------------------- Log request to console
 app.use(express.urlencoded({ extended: true}));//<------- Pass incoming urlencoded data into "req.body"
 
 
-const site_title = 'Node Blog';
-
-
-// Connect to mongodb
+// 1. Connect to DB (mongodb, using "mongoose")
+// 2. Begin watching
 const targetDbClusterName = 'Cluster0';
 const targetDbName = 'node-blog';
 const targetDbUser = 'kquilty_db_user';
@@ -36,99 +35,28 @@ mongoose.connect(dbURI)
     });
 
 
-/* Example: Custom log (now handled by morgan above)
-
-    app.use((req, res, next) => {
-    console.log('new request made:');
-    console.log('host: ', req.hostname);
-    console.log('path: ', req.path);
-    console.log('method: ', req.method);
-    next();//tell the browser not to hang and do nothing (crazy that this is needed)
-    });
-
-*/
-
-
-
-
-
-
-
-
-
-
-// SANDBOX routes
-/*
-        app.get('/add-blog', (req, res) => {
-            const blog = new Blog();
-            blog.title = 'my title';
-            blog.snippet = 'a snippet here';
-            blog.body = 'here is the body';
-
-            blog.save()
-            .then((result) => {
-                console.log("Saved new record.")
-                console.log(result);
-                res.send(result);
-            })
-            .catch((error) => {
-                console.log("FAILED TO SAVE");
-                console.log(error);
-            });
-
-        });
-        app.get('/all-blogs', (req, res) => {
-            Blog.find()
-            .then((result) => {
-                console.log(result);
-                res.send(result);
-            })
-            .catch((error) => {
-                console.log("FAILED TO FIND");
-                console.log(error);
-            });
-        });
-        app.get('/one-blog', (req, res) => {
-            Blog.findById('68dd4cee6348009fec37acee')
-            .then((result) => {
-                console.log(result);
-                res.send(result);
-            })
-            .catch((error) => {
-                console.log("FAILED TO FIND");
-                console.log(error);
-            });
-        });
-*/
-
-
-
-
-
-// Routes
+// ROUTES --------------------------------------------------------------------------------------
+// Top Level Routes
 app.get('/', (req, res) => {
     res.redirect('/blogs');
 });
-
 app.get('/about', (req, res) => {
-    res.render('about', {
-        site_title
-    });
+    res.render('about');
 });
 
 // Blog routes
 app.use('/blogs', blogRoutes);
 
-// Redirect some old pages
+// RedirectS
 app.get('/about-us', (req, res) => {
     res.redirect('/about');
 });
 
 
-// If WE GOT TO THIS POINT, use this 404 page
+// 404 FALLBACK ---------------------------------------------------------------------------------
+// If the code has reached here, use this 404 page
 app.use((req, res) => {
     res.status(404).render('404', {
-        site_title,
         attempted_url: req.url
     });
 });
