2b43b6c22be91031cf0d935b1fb55462c611bbf8
Kyle Quilty <kquilty@gmail.com>
2025-10-01 11:23:19 -0400

    - use logging middleware "morgan" - use DB middleware "mongoose" - connected to mongoDB (on Mongo Atlas)

--- app.js
+++ app.js
@@ -1,17 +1,46 @@
 const express = require('express');
+const morgan = require('morgan');//<--------- middleware for logging
+const mongoose = require('mongoose');//<----- middleware for better DB interaction
 
 const app = express();
 
 app.set('view engine', 'ejs');// by default, looks in "./views"
 
-app.use(express.static('public'));
-
-app.listen(3000);
+// middleware & static files
+app.use(express.static('public'));// Make "/public" accessable to the browser
 
 
 const site_title = 'Node Blog';
 
+// Connect to mongodb
+const dbURI = 'mongodb+srv://kquilty_db_user:ArwSpZ4KLwqzekZK@cluster0.o5gyhoz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+console.log("connecting to DB...");
+mongoose.connect(dbURI)
+    .then((result) => {
+        console.log("Connected");
+        
+        // Safe to begin listening
+        app.listen(3000);
+    })
+    .catch((error) => {
+        console.log("CONNECTION FAILED");
+        console.log(error);
+    });
 
+/* Example: Custom log
+
+    app.use((req, res, next) => {
+    console.log('new request made:');
+    console.log('host: ', req.hostname);
+    console.log('path: ', req.path);
+    console.log('method: ', req.method);
+    next();//tell the browser not to hang and do nothing (crazy that this is needed)
+    });
+
+*/
+
+// Log request to console
+app.use(morgan('dev'));
 
 // Routes
 app.get('/', (req, res) => {
